64fde5d064f6d3964c5381f517abd97c7d11e3fe
Camifornication <camiel.vannek@student.hu.nl>
2023-12-04 10:23:12 +0100

    Begin van de Gui

--- python-applicatie.py
+++ python-applicatie.py
@@ -1,1 +1,14 @@
-print("hello world")
+import json
+from tkinter import *
+def gui():
+    root = Tk()
+    root.geometry("1200x600")
+    root.title("BeginGui")
+    # root.resizable(0,0)
+    lbl1 = Label(root, text="test")
+    lbl1.pack()
+
+
+    root.mainloop()
+
+gui()
