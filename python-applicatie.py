49afb6eb321c35b04ff9cde15ad8d9ca70ffb3c0
JortFreriksen <Jort.freriksen@student.hu.nl>
2024-01-03 21:09:13 +0100

    Inlog scherm
    Inlog scherm die je doorstuurt naar een nieuw scherm die je welkom heet.
    functie om de personaname te krijgen.

--- python-applicatie.py
+++ python-applicatie.py
@@ -2,6 +2,11 @@ import json
 import requests
 from tkinter import *
 
+jort_ID = 76561198424424214
+camiel_ID = 76561199075949807
+abi_ID = 76561199499642445
+joeri_ID = 76561198811411788
+
 steam_api_key = "61D1D964724B68FC9F340D584CD500E3"
 user_id = "76561198424424214"
 
@@ -37,16 +42,22 @@ def get_json_file(steam_api_link):
     data = response.json()
     return json.dumps(data, indent=2)
 
-def get_friendlist(user_id):
-    return get_json_file(f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={steam_api_key}&steamid={user_id}&relationship=friend")
+def get_friendlist(steamid):
+    return get_json_file(f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={steam_api_key}&steamid={steamid}&relationship=friend")
 
 # print(get_friendlist(user_id))
 
-def get_player_info():
-    return get_json_file(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={steam_api_key}&steamids={user_id}")
+def get_player_info(steamid):
+    return get_json_file(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={steam_api_key}&steamids={steamid}")
 
-def last_logoff_friendlist():   #get each friend of friendlist with last logoff
-    response = requests.get(f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={steam_api_key}&steamid={user_id}&relationship=friend")
+def get_personaname(steamid):
+    response = requests.get(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={steam_api_key}&steamids={steamid}")
+    data = response.json()
+    personaname = data['response']['players'][0]['personaname']
+    return personaname
+
+def last_logoff_friendlist(steamid):   #get each friend of friendlist with last logoff
+    response = requests.get(f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={steam_api_key}&steamid={steamid}&relationship=friend")
     data = response.json()
     steamid_list = []
     for friend in data['friendslist']['friends']:
@@ -59,18 +70,6 @@ def last_logoff_friendlist():   #get each friend of friendlist with last logoff
         lastlogoff = data['response']['players'][0]['lastlogoff']
         print(f"User: {personaname}, Last log-off: {lastlogoff}")
 
-def gui():
-    root = Tk()
-    root.geometry("1200x600")
-    root.title("BeginGui")
-    # root.resizable(0,0)
-    lbl1 = Label(root, text="test")
-    lbl1.pack()
-
-
-    root.mainloop()
-
-# gui()
 
 def sorted():
     game_name_list = []
@@ -84,3 +83,38 @@ def sorted():
 
 # sorted()
 
+def main_gui():
+    steamid = steamid_input.get()
+
+    root1.destroy()
+    root2 = Tk()
+    root2.geometry("1200x600")
+    root2.title("Main screen")
+
+    personaname = get_personaname(steamid)
+
+    lbl = Label(master=root2, text=f"Welcome {personaname}")
+    lbl.pack()
+
+def login_gui():
+    global root1
+    global steamid_input
+    root1 = Tk()
+    root1.geometry("1200x600")
+    root1.title("Login")
+    # root.resizable(0,0)
+    root1.configure(bg="#1b2838")
+
+    lbl1 = Label(master=root1, text="What is your steam ID?", bg="#66c0f4")
+    lbl1.pack(pady=20)
+
+    steamid_input = Entry(master=root1)
+    steamid_input.pack()
+
+    confirm_btn = Button(master=root1, text="Confirm", command=main_gui)
+    confirm_btn.pack(pady=20)
+
+
+    root1.mainloop()
+
+login_gui()
